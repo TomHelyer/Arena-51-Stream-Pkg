80b600e55946494fd57710fee651f4077bb659b8
tomhelyer <tom@arena51gaming.co.uk>
2023-05-21 23:27:33 +0100

    added scoreboard game overlay

--- frontend/src/Pages/Scoreboard/scoreboard.tsx
+++ frontend/src/Pages/Scoreboard/scoreboard.tsx
@@ -1,5 +1,10 @@
+import { relative } from 'path';
 import { useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
+import push from './arrow-right-fill.svg';
+import def from './shield-fill.svg';
+import att from './sword-fill.svg';
+const image = require('./Capture.PNG');
 
 enum matchStates{
     Home = 1,
@@ -8,61 +13,108 @@ enum matchStates{
     NoResult = ""
 }
 
+
+//d63750
+//2fbbde
+
 const createStyles = createUseStyles({
     frame: {
         position: "absolute",
         display: "flex",
         flexDirection: "row",
         width: "100%",
+        height: "100%",
         fontFamily: "technovier",
-        fontSize: "200%",
+        fontSize: "150%",
         textAlign: "center",
         justifyContent: "space-between",
         aspectRatio: `${1920/1080}`,
+        backgroundImage: `url(${image})`,
+        backgroundSize: "100%",
+        boxSizing: "border-box",
+        paddingTop: "0.8%",
+        paddingLeft: "2.25%",
+        paddingRight: "2.25%",
     },
-    home: {
+    homeCol: {
+        backgroundColor: "#2fbbde"
+    },
+    awayCol: {
+        backgroundColor: "#d63750",
+    },
+    team: {
         display: "flex",
+        justifyContent: "space-between",
         flexDirection: "row",
-        width: "35%",
-        height: "5%",
-        backgroundColor: "blue",
+        width: "28.55%",
+        height: "3.5%",
+        boxShadow: "-5px 10px 20px black",
     },
     away: {
         display: "flex",
+        justifyContent: "space-between",
         flexDirection: "row-reverse",
-        width: "35%",
-        height: "5%",
-        backgroundColor: "red",
+        width: "30%",
+        height: "3.5%",
     },
     logo:{
         backgroundColor: "white",
-        width: "10%",
+        height: "100%",
         aspectRatio: 1,
-    },
-    img: {
-        width: "100%",
-        height: "100%"
-    },
-    teamName: {
-        width: "70%",
-        backgroundColor: "white",
         display: "flex",
-        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
     },
+    img: {
+        width: "90%",
+        height: "90%",
+    },
+    transImg: {
+        opacity: "0.3",
+        width: "90%",
+        filter: "blur(2px)",
+    },
+    backgroundImg: {
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+    },
+    teamName: {
+        backgroundColor: "white",
+        borderLeft: "2px solid",
+        borderRight: "2px solid",
+    },
+    textOnTop: {
+        position: "relative",
+        top: "-100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
     score: {
         width: "10%",
-        fontSize: "130%",
+        fontSize: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
     },
     icon: {
-        width: "10%",
+        height: "100%",
+        aspectRatio: 1,
         backgroundColor: "white",
-        fontSize: "160%",
+        borderLeft: "2px solid",
     },
     text: {
         padding: "0",
         margin: "0"
+    },
+    flip: {
+        transform: "scaleX(-1)"
     }
 });
 
@@ -112,36 +164,43 @@ const Scoreboard = () => {
 
     return (    
         <div className={styles.frame}>
-            <div className={styles.home}>
+            <div className={styles.team}>
                 <div className={styles.logo}>
-                    <img className={styles.img} src={`data:image/png;base64, ${images[0]}`} alt={match.home.name}/>
+                    <img className={styles.img} src={`data:image/png;base64, ${images[0]}`} alt={match.home.name[0]}/>
                 </div>
                 <div className={styles.teamName}>
-                    <p className={styles.text}>{match.home.name}</p>
+                    <div className={styles.backgroundImg}>
+                        <img className={styles.transImg} src={`data:image/png;base64, ${images[0]}`} alt={""}/>
+                    </div>
+                    <div className={styles.textOnTop}>
+                        <p className={styles.text}>{match.home.name}</p>
+                    </div>
                 </div>
-                <div className={styles.score}>
+                <div className={`${styles.score} ${styles.homeCol}`}>
                     <p className={styles.text}>{score[0]}</p>
                 </div>
                 <div className={styles.icon}>
-                    <i className="ri-arrow-right-fill"></i>
+                    <img src={push} alt=""/>
                 </div>
             </div>
 
-            <div className={styles.away}>
-                <div className={styles.logo}>
-                    <img className={styles.img} src={`data:image/png;base64, ${images[1]}`} alt={match.away.name}/>
+            <div className={`${styles.team} ${styles.flip}`}>
+                <div className={styles.logo }>
+                    <img className={`${styles.img} ${styles.flip}`} src={`data:image/png;base64, ${images[1]}`} alt={match.away.name[0]}/>
                 </div>
-
-                <div className={styles.teamName}>
-                    <p className={styles.text}>{match.away.name}</p>
+                <div className={`${styles.teamName} ${styles.flip}`}>
+                    <div className={styles.backgroundImg}>
+                        <img className={styles.transImg} src={`data:image/png;base64, ${images[1]}`} alt={""}/>
+                    </div>
+                    <div className={styles.textOnTop}>
+                        <p className={styles.text}>{match.away.name}</p>
+                    </div>
                 </div>
-
-                <div className={styles.score}>
-                    <p className={styles.text}>{score[1]}</p>
+                <div className={`${styles.score} ${styles.awayCol}`}>
+                    <p className={`${styles.text} ${styles.flip}`}>{score[1]}</p>
                 </div>
-
                 <div className={styles.icon}>
-                    <i className="ri-arrow-left-fill"></i>
+                    <img src={push} alt=""/>
                 </div>
             </div>
         </div>
