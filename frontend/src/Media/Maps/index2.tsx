2f3a0c907baec842f681b04c8958e5b9aaa0707b
GeneralHuber <luki-hu@gmx.ch>
2023-08-27 15:40:36 +0200

    Create mapLookupAdvanced object in preparation of new map scene. Add all available competetive maps to the object and divide it in gamemodes.

---
+++ frontend/src/Media/Maps/index2.tsx
@@ -0,0 +1,76 @@
+import AntarcticPeninsula from './AntarcticPeninsula.webp';
+import BlizzardWorld from './BlizzardWorld.webp';
+import Busan from './Busan.webp';
+import CircuitRoyale from './CircuitRoyale.webp';
+import Colosseo from './Colosseo.webp';
+import Control from './Control.webp';
+import Dorado from './Dorado.webp';
+import Eichenwalde from './Eichenwalde.webp'
+import Escort from './Escort.webp';
+import Esparanca from './Esparanca.webp';
+import Flashpoint from './FlashPoint.png';
+import Havana from './Havana.webp';
+import Hollywood from './Hollywood.webp';
+import Hybrid from './Hybrid.webp';
+import KingsRow from './KingsRow.webp';
+import Ilios from './Ilios.webp';
+import LijiangTower from './LijiangTower.webp';
+import Midtown from './Midtown.webp';
+import Nepal from './Nepal.webp';
+import NewJunkCity from './NewJunkCity.jpg';
+import NewQueenStreet from './NewQueenStreet.webp';
+import Numbani from './Numbani.webp';
+import Oasis from './Oasis.webp';
+import Rialto from './Rialto.webp';
+import Route66 from './Route_66.webp';
+import ShambaliMonestary from './ShambaliMonestary.webp';
+import Suravasa from './Suravasa.webp';
+import Paraiso from './Paraiso.webp';
+import WatchpointGibraltar from './Gibraltar.webp';
+
+
+const mapLookupAdvanced: any = {
+    control: {
+        AntarcticPeninsula,
+        Busan,
+        Control,
+        Ilios,
+        LijiangTower,
+        Nepal,
+        Oasis,
+    },
+    escort: {
+        CircuitRoyale,
+        Dorado,
+        Escort,
+        Havana,
+        Rialto,
+        Route66,
+        ShambaliMonestary,
+        WatchpointGibraltar,
+    },
+    flashpoint: {
+        Flashpoint,
+        NewJunkCity,
+        Suravasa,
+    },
+    hybrid: {
+        BlizzardWorld,
+        Eichenwalde,
+        Hollywood,
+        Hybrid,
+        KingsRow,
+        Midtown,
+        Numbani,
+        Paraiso,
+    },
+    push: {
+        // still needs the default push icon, idk where to find. fml
+        Colosseo,
+        Esparanca,
+        NewQueenStreet,
+    }
+
+}
+
+export default mapLookupAdvanced;
